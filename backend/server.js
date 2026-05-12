81f8c7ff391a082e4f8fcb2663773c11a13728c3
tusharpatel01 <tushar2212099@akgec.ac.in>
2026-05-12 18:05:20 +0530

    morgan added in server.js

--- backend/server.js
+++ backend/server.js
@@ -42,6 +42,7 @@ app.use("/api", apiLimiter);
 app.use(express.json({ limit: "10mb" }));
 app.use(express.urlencoded({ extended: true, limit: "10mb" }));
 app.use(cookieParser());
+app.use(morgan("dev"));
 app.use(mongoSanitize());
 if (process.env.NODE_ENV === "development") app.use(morgan("dev"));
 app.use((req, _res, next) => { req.io = io; next(); });
